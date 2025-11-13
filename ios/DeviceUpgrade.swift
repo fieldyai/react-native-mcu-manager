980716966cf908e40f1cd1aa6f6b031d1640c2cf
mkrupskis <martynaskrupskis@gmail.com>
2025-11-13 16:39:14 +0200

    wait even longer

--- ios/DeviceUpgrade.swift
+++ ios/DeviceUpgrade.swift
@@ -120,7 +120,7 @@ class DeviceUpgrade {
 
       self.dfuManager!.logDelegate = self.logDelegate
 
-      DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+      DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
         do {
           try self.dfuManager!.start(images: images, using: config)
         } catch {
